bb5b7e48155cd9917b6836746e28dd8262522d54
seb <seb@ruse.tech>
2022-12-23 12:58:36 -0800

    fmt

--- interpreter/interpreter.go
+++ interpreter/interpreter.go
@@ -25,32 +25,40 @@ func NewBrainfuck(code string) (*Brainfuck, error) {
 }
 
 func (bf *Brainfuck) Interpret() {
-	bf.ip = 0                          // set instruction pointer to 0
-	for bf.ip < len(bf.Instructions) { // this might cause a bug if the loop is at the end of the program
-		op := bf.Instructions[bf.ip] // get the current instruction
+	bf.ip = 0
+
+	for bf.ip < len(bf.Instructions) {
+		op := bf.Instructions[bf.ip]
 		bf.ip++
+
 		switch op.Token.TokenType {
+
 		case lexer.INCREMENT:
 			bf.Memory[bf.dp]++
+
 		case lexer.DECREMENT:
 			bf.Memory[bf.dp]--
+
 		case lexer.SHIFT_LEFT:
 			bf.dp--
+
 		case lexer.SHIFT_RIGHT:
 			bf.dp++
+
 		case lexer.OUTPUT:
 			fmt.Print(bf.Memory[bf.dp])
+
 		case lexer.INPUT:
 			var input int
 			_, _ = fmt.Scan(&input)
 			bf.Memory[bf.dp] = input
+
 		case lexer.OPEN_LOOP:
 			if bf.Memory[bf.dp] <= 0 {
 				bf.ip = op.Jump + 1
 			}
 		case lexer.CLOSE_LOOP:
 			bf.ip = op.Jump
-
 		}
 	}
 }
